8416452e1354d6377cc66da18421b8e55e7c6826
sanjeev <sanjeev@sanjeevs-MacBook-Pro.local>
2017-07-13 12:42:21 +0530

    Refactor ParkingArea class <Sanjeev, Nouman>

--- src/main/java/parking/ParkingLot.java
+++ src/main/java/parking/ParkingLot.java
@@ -1,25 +1,24 @@
 package parking;
-
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by sanjeev on 13/07/17.
  */
 public class ParkingLot {
-    private int size;
-    List<Car> parkedCars = new ArrayList<Car>();
+    private final int size;
+    private final Set<Car> parkedCars;
 
     public ParkingLot(int size) {
+        parkedCars =  new HashSet<Car>();
         this.size = size;
     }
 
     public boolean accept(Car car) {
-        if (isFull() || isCarParked(car))
+        if (isFull())
             return false;
 
-        parkedCars.add(car);
-        return true;
+        return parkedCars.add(car);
     }
 
     public boolean release(Car car) {
